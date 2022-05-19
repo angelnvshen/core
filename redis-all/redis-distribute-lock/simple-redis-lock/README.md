dd599e080448a5ba60df3cf62f23990f549fcab8
liang.chen <angelnvshen@163.com>
2022-05-19 16:42:28 +0800

    redis pipeline with spring-data-redis source'code review

--- redis-all/redis-distribute-lock/simple-redis-lock/README.md
+++ redis-all/redis-distribute-lock/simple-redis-lock/README.md
@@ -1,6 +1,16 @@
 #### redis 分布式锁
 #### redis pipeline
 
+pipeline顾名思义就是流水线操作，像http 1.1也开始支持pipeline来把多个HTTP请求放到一个TCP连接中一 一发送，而在发送过程中不需要等待服务器对前一个请求的响应，在redis client中使用pipeline的主要目的也与此相同，打包请求的同时减少了很多网络IO。在lettuce中的底层实现是将请求中的多个command先放到socket buffer中，然后统一flush出去。
 
+##### **1 lettuce原生的pipeline**
+
+关闭了auto flush选项，它会调用 writeToBuffer(command)方法，将command先flush到socket buffer中，在后面调用 commands.flushCommands()方法时才真正地执行flush操作。
+
+##### **2 spring-data-redis包装后的lettuce的pipeline**
+
+在使用redisTemplate的时候，从来都没有设置过autoFlushCommands，没错，这里它为true，也就是说每来set一次就会flush一次，这也就解释了为什么在第一个set操作之后断点在redis中可以查到第一条已经set成功的原因。
+
+**也就是说，直接使用redisTemplate来操作pipeline时它还是一条条地去操作的，是一个伪批操作。** 
 
 #### todo redis 配置pool, 单机，sentinel，集群，docker, docker file
--- redis-all/redis-distribute-lock/simple-redis-lock/src/main/java/own/stu/redis/pipeline/PipelineController.java
+++ redis-all/redis-distribute-lock/simple-redis-lock/src/main/java/own/stu/redis/pipeline/PipelineController.java
@@ -1,12 +1,25 @@
 package own.stu.redis.pipeline;
 
+import com.google.common.collect.Lists;
+import io.lettuce.core.LettuceFutures;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands;
+import io.lettuce.core.resource.ClientResources;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnection;
 import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StopWatch;
@@ -17,6 +30,7 @@ import own.stu.redis.logAop.LogAnnotation;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @RequestMapping("pipeline")
@@ -45,9 +59,79 @@ public class PipelineController {
         return "SUCCESS";
     }
 
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @LogAnnotation
+    @RequestMapping("test_pipeline_no_spring")
+    public String testPipelineNoSpring() {
+
+        //RedisClient client = RedisClient.create(ClientResources.create(), RedisURI.create("redis://qtshe654321@116.62.12.66:6379/0"));
+        RedisClient client = RedisClient.create(ClientResources.create(), RedisURI.create("redis://" + host + ":6379/0"));
+        StatefulRedisConnection<String, String> connection = client.connect();
+
+        RedisAsyncCommands<String, String> commands = connection.async();
+        // disable auto-flushing
+        commands.setAutoFlushCommands(false);
+        // perform a series of independent calls
+        List<RedisFuture<?>> futures = Lists.newArrayList();
+        for (int i = 1; i < 50; i++) {
+            futures.add(commands.set("mmm:" + i, "value-" + i));
+            futures.add(commands.expire("mmm:" + i, 3600));
+        }
+
+        // write all commands to the transport layer
+        // 真正地flush操作
+        commands.flushCommands();
+        // synchronization example: Wait until all futures complete
+        boolean result = LettuceFutures.awaitAll(5, TimeUnit.SECONDS,
+                futures.toArray(new RedisFuture[futures.size()]));
+
+        // later
+        connection.close();
+        client.shutdown();
+
+        return "SUCCESS";
+    }
+
+    @LogAnnotation
+    @RequestMapping("test_pipeline_opti")
+    public String testPipelineOpti() {
+        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
+        LettuceConnection connection = null;
+        try {
+            connection = (LettuceConnection) RedisConnectionUtils.getConnection(connectionFactory);
+            //LettuceConnection connection = (LettuceConnection)redisTemplate.getConnectionFactory().getConnection();
+            RedisClusterAsyncCommands<byte[], byte[]> commands = connection.getNativeConnection();
+            commands.setAutoFlushCommands(false);
+            List<RedisFuture<?>> futures = Lists.newArrayList();
+            for (int i = 0; i < 50; i++) {
+                futures.add(commands.set(("mmm:" + i).getBytes(), ("value-" + i).getBytes()));
+                futures.add(commands.expire(("mmm:" + i).getBytes(), 3600));
+            }
+
+            // write all commands to the transport layer
+            commands.flushCommands();
+
+            // synchronization example: Wait until all futures complete
+            boolean result = LettuceFutures.awaitAll(5, TimeUnit.SECONDS,
+                    futures.toArray(new RedisFuture[futures.size()]));
+        } finally {
+            if (connection != null) {
+                RedisConnectionUtils.releaseConnection(connection, connectionFactory);
+            }
+        }
+        return "SUCCESS";
+    }
+
     @LogAnnotation
     @RequestMapping("test_pipeline")
     public String testPipeline() {
+
+        // redisTemplate.delete("test0:*"); 这样模糊删除无效，需要eval( lua脚本）或者 pipeline
 
         List<Integer> recordList = new ArrayList<>();
 
